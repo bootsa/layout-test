2f4745ade89191b212bbb9159141420e43f20934
Steph Tyszka <steph@bootsa.net>
2022-01-09 12:28:23 +0100

    layout test

---
+++ src/routes/[section]/__layout.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+	export async function load(data) {
+		return {
+			props: {
+				data
+			}
+		};
+	}
+</script>
+
+<script>
+	export let data;
+	console.log({ data });
+</script>
+
+<h2>2</h2>
+<pre>
+  {JSON.stringify(data, null, 2)}
+</pre>
+
+<slot />
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,22 @@
+<script context="module">
+	export async function load(data) {
+		return {
+			props: {
+				data
+			}
+		};
+	}
+</script>
+
+<script>
+	export let data;
+	console.log({ data });
+</script>
+
+<h2>1</h2>
+
+<pre>
+  {JSON.stringify(data, null, 2)}
+</pre>
+
+<slot />
